30e51b8cdc2b1799b131994dfa1453eebee31b36
astianmuchui <astianmuchui@gmail.com>
2024-03-18 09:34:02 +0300

    lock boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 You have n number of locked boxes in front of you. Each box is numbered
 sequentially from 0 to n - 1 and each box may contain keys to the other
