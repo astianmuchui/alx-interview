df298b5a0d50033593f4ac79206cadb93a952ea6
astianmuchui <astianmuchui@gmail.com>
2024-03-25 15:29:26 +0300

    PEP8 Validation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,11 +7,13 @@ Given a number n, write a method that calculates the fewest number
 of operations needed to result in exactly n H characters in the file.
 """
 
+
 def minOperations(n):
     """"
     Method that calculates the fewest number of operations needed to
     result in exactly n H characters in the file.
     """
+
     if n <= 1:
         return 0
     for i in range(2, n + 1):
