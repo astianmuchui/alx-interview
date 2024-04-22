dc280e947de1abcaaed0eb775f8911b948f694a8
astianmuchui <astianmuchui@gmail.com>
2024-04-22 16:25:15 +0300

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,14 +4,13 @@ const request = require('request');
 const arg = process.argv[2];
 const uri = 'https://swapi-api.hbtn.io/api/films/' + arg;
 
-
 request(uri, async (error, res, body) => {
   if (error) {
     console.log(error);
   }
-  for (const char_id of JSON.parse(body).characters) {
+  for (const char of JSON.parse(body).characters) {
     await new Promise((resolve, reject) => {
-      request(char_id, (error, res, body) => {
+      request(char, (error, res, body) => {
         if (error) {
           reject(error);
         }
